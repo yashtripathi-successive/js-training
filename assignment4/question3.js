7f191e1907409351ae943018554d2fcaf891a35e
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-10 13:18:35 +0530

    Added Questions on Assignment4

--- assignment4/question3.js
+++ assignment4/question3.js
@@ -1,3 +1,6 @@
+//Q3 - Write a program to give count of variables in a string ("abcabcdabbcc" => "a3b4c4d1")
+
+
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
