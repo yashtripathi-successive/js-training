f272e246c48d14007c117c8d8874c05172e5101b
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-10 13:30:44 +0530

    Added Questions on Assignment5

--- assignment5/question5.js
+++ assignment5/question5.js
@@ -1,3 +1,10 @@
+//Q5 - Write a program flattenObject that takes an object with nested properties and returns a flat object with the nested properties transformed into dot-separated properties. The function should handle nested objects and arrays.
+//  const obj = {
+//    a: 1,
+//    b: { c: 2, d: [3, 4] }
+//  };
+//  Output: { 'a': 1, 'b.c': 2, 'b.d.0': 3, 'b.d.1': 4 }
+
 
 const obj = {
     a:1,
