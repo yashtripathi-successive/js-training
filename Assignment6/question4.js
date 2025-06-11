a1688c0b0220ab6176704a4cab2fa89f3641184d
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-11 11:21:28 +0530

    Added Questions on Assignment6

--- Assignment6/question4.js
+++ Assignment6/question4.js
@@ -1,4 +1,8 @@
-//Q4
+//Q4 - Create a class called `Person` that has properties for `firstName`, `lastName`, and `age`. 
+// The class should also have a method called `fullName` that returns the person's full name.
+// Additionally, the class should have a method called `averageAge` that takes in an array of `Person` objects and returns the average age of all the people in the array.
+
+
 
 class Person{
 constructor(first_name,last_name,age){
