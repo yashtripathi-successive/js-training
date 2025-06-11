d9b2dbbb538ce0c2a8f385aea752c3f1b31a345b
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-11 16:59:24 +0530

    Update question3.js

--- assignment5/question3.js
+++ assignment5/question3.js
@@ -1,12 +1,14 @@
 //Q3 - Write a program to remove all the id key from array of object ({name:"John", age:27, id:1})
 
 const obj = [
-    {name:"yash", age:21,id:1},
-    {name:"anany", age:22,id:2}
-]
+  { name: "yash", age: 21, id: 1 },
+  { name: "anany", age: 22, id: 2 },
+];
 
 const newObj = obj.map(({id,...rest}) => {
-      return rest;
-})
+  delete id;         
+  return rest;
+});
 
 console.log(newObj);
+
