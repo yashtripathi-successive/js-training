f2c6ef20b20b928c26fe9a33a522645cb24642f0
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-11 16:50:51 +0530

    Update question9.js

--- assignment4/question9.js
+++ assignment4/question9.js
@@ -1,14 +1,24 @@
 //Q9 - Write a program to find the last duplicate index in an array
 
 
-let arr = [1,2,1,2,1];
-let seen = new Set();
+let arr = [1, 2, 1, 2, 1,5,5,8,2,7,8];
 
-for(let i=0;i<arr.length-1;i++){
-    if(!seen.has(arr[i])){
-        seen.add(arr[i]);
-    }
-    else{
-     console.log(`${arr[i]} has a last index at ${arr.lastIndexOf(arr[i])}`) 
-    }
+
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] !== 'number' || isNaN(arr[i])) {
+    console.error(`Invalid element found`);
+    process.exit(1);
+  }
+}
+
+const freq = {};
+for (const num of arr) {
+  freq[num] = freq[num] ? freq[num] + 1 : 1;
+}
+
+
+for (const num in freq) {
+  if (freq[num] > 1) {
+    console.log(`${num} has a last index at ${arr.lastIndexOf(Number(num))}`);
+  }
 }
