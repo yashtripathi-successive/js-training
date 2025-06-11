59bd0f2b3267a2a0825945ac43cb7ea5fa8fe2eb
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-11 16:41:23 +0530

    Update question8.js

--- assignment4/question8.js
+++ assignment4/question8.js
@@ -1,17 +1,22 @@
 //Q8 - Write a program to remove dupliacte elements from an array
 
-
-let arr = [1,2,1,2,3];
+let arr = [1, 2, 1, 2, 3];
 let seen = new Set();
-
-for(let i=0;i<arr.length;i++){
-    if(!seen.has(arr[i])){
-        seen.add(arr[i]);
-    }
-}
-
 let ans = "";
-for(let x of seen){
-    ans+=x+" ";
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] !== 'number' || isNaN(arr[i])) {
+    console.error(`Invalid element found`);
+   return;
+  }
+   
+  if (!seen.has(arr[i])) {
+    seen.add(arr[i]);
+  }else{
+      ans+=arr[i]+' ';
+  }
 }
-console.log(ans);
+
+
+
+console.log(ans.trim());
+
