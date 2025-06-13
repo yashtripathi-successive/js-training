fee57bfef127312ee7692aa70f4fd8776f6351f3
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-13 12:07:22 +0530

    Update question1.js

--- assignment5/question1.js
+++ assignment5/question1.js
@@ -1,10 +1,17 @@
 //Q1 - Write a program to iterate over object.
 
 const obj = {
-    name:"yash",
-    age:21
+    first_name: "yash",
+    last_name: "tripathi"
+};
+
+
+if (obj && typeof obj === 'object') {
+        for (let key in obj) {
+                console.log(`${key} -> ${obj[key]}`);
+            
+        }
+    }  else {
+    console.log("Invalid input");
 }
 
-for(let key in obj){
-    console.log(`obj has : ${key} ->  ${obj[key]} `)
-}
