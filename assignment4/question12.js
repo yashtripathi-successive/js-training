1c41509dbb20371d867c1e889c879d293bf1c9fd
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-13 10:17:05 +0530

    Update question12.js

--- assignment4/question12.js
+++ assignment4/question12.js
@@ -1,30 +1,42 @@
 
 //Q12 - Write a program to display intersection of two array
 
-let arr1 = [1,2,3,4,5];
-let arr2 = [1]; 
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [1, 5];
 
-let seen1 = new Set();
-let seen2 = new Set();
 
-for(let i=0;i<arr1.length;i++){
-    if(!seen1.has(arr1[i])){
-        seen1.add(arr1[i]);
+function isValidArray(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Input is not an array.");
+  } else if (arr.length === 0) {
+    throw new Error("Array is empty.");
+  } else if (!arr.every((item) => typeof item === 'number' && !isNaN(item))) {
+    throw new Error("Array must contain only valid numbers.");
+  } else{
+      return arr;
+  }
+}
+
+
+try{
+  if(isValidArray(arr1) && isValidArray(arr2)) {
+  
+  let seen1 = new Set([...arr1]);
+  let seen2 = new Set([...arr2]);
+
+  let ans = "";
+
+  for (let x of seen1) {
+    if (seen2.has(x)) {
+      ans += x + " ";
     }
+  }
+
+  console.log(ans.trim()); 
+}
+}
+catch(error){
+    console.log('error');
 }
 
-for(let i=0;i<arr2.length;i++){
-    if(!seen2.has(arr2[i])){
-        seen2.add(arr2[i]);
-    }
-}
 
-let ans = "";
-
-for(let x of seen1){
-   if(seen2.has(x)){
-    ans+=x+" ";
-   }
-}
-
-console.log(ans);
