27a0da692fb5b74b5a8e833aaeb03c8b6194f4d9
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-13 11:54:04 +0530

    Update question8.js

--- assignment4/question8.js
+++ assignment4/question8.js
@@ -3,12 +3,14 @@
 let arr = [1, 2, 1, 2, 3];
 let seen = new Set();
 let ans = "";
+if (!Array.isArray(arr)) {
+  console.log("Input is not an array.");
+} else if (arr.length === 0) {
+  console.log("Array is empty.");
+} else if (arr.some(ele => typeof ele !== "number" || isNaN(ele))) {
+  console.log("string found.");
+} else {
 for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] !== 'number' || isNaN(arr[i])) {
-    console.error(`Invalid element found`);
-   return;
-  }
-   
   if (!seen.has(arr[i])) {
     seen.add(arr[i]);
   }else{
@@ -16,7 +18,6 @@ for (let i = 0; i < arr.length; i++) {
   }
 }
 
-
-
-console.log(ans.trim());
+console.log(ans);
+}
 
