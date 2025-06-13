fa52f312c41b95c912c3ab318dfd9907b38610ad
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-13 12:17:11 +0530

    Update question2.js

--- assignment5/question2.js
+++ assignment5/question2.js
@@ -1,13 +1,19 @@
 //Q2 - Write a program to sort an array of object on the basis of age 
 
 const obj = [
-    {name:"yash", age:21},
-    {name:"anany", age:22}
-]
+    { name: "yash", age: 21 },
+    { name: "anany", age: 20 }
+];
 
+const isValid = obj.every((item) =>  typeof item === 'object' && item !== null && 'age' in item && 
+    typeof item.age == 'number' && 'name' in item && typeof item.name === 'string');
+if (isValid) {
+     obj.sort((a, b) => a.age - b.age);
 
-obj.sort((a,b)=> a.age - b.age)
-
-obj.forEach((val) => {
-console.log(`${val.name} -> ${val.age}`);
-})
+     obj.forEach(val => {
+            console.log(`${val.name} -> ${val.age}`);
+        });
+}
+else{
+    console.log('error');
+}
