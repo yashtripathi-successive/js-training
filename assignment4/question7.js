e25afc96f5d363e1a785d1a5a8c5140f15c1778d
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-17 17:04:10 +0530

    Update question7.js

--- assignment4/question7.js
+++ assignment4/question7.js
@@ -1,25 +1,23 @@
 //Q7 - Write a program to find index of duplicate elements in an array
 
 
+let arr = [1,'a','b','a',1];
 let seen = new Set();
-
-let arr = [1, 2, 3, 1,2,3,5,5];
-let res = "";
+let res = [];
 
 if (!Array.isArray(arr)) {
   console.log("Input is not an array.");
 } else if (arr.length === 0) {
   console.log("Array is empty.");
-} else if (arr.some(ele => typeof ele !== "number" || isNaN(ele))) {
-  console.log("string found.");
 } else {
-  for (let i = 0; i < arr.length; i++) {
-  if (!seen.has(arr[i])) {
-    seen.add(arr[i]);
-  } else {
-    res += i + " ";
-  }
+  arr.map((ele, index) => {
+    if (seen.has(ele)) {
+      res.push(index);
+    } else {
+      seen.add(ele);
+    }
+  });
+
+  console.log(res);
 }
 
-console.log(res);
-}
