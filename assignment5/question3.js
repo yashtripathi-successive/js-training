efc17baa96e620406413ecca3366425dc2383835
yashtripathi-successive <yash.tripathi@successive.tech>
2025-06-18 11:16:02 +0530

    Update question3.js

--- assignment5/question3.js
+++ assignment5/question3.js
@@ -1,24 +1,24 @@
 //Q3 - Write a program to remove all the id key from array of object ({name:"John", age:27, id:1})
 
-const obj = [
+const arr = [
   { name: "yash", age: 21, id: 1 },
   { name: "anany", age: 22, id: 2 }
 ];
 
 
-const isValid = obj.every((item) =>
+const isValid = arr.every((item) =>
   typeof item === 'object' && item !== null &&
   'name' in item && typeof item.name === 'string' &&
   'age' in item && typeof item.age === 'number' &&
   'id' in item && typeof item.id === 'number');
 
 if (isValid) {
-  const newObj = obj.map(({ id, ...rest }) => {
+  const newArr = arr.map(({ id, ...rest }) => {
       delete id;
       return rest
   });
 
-  console.log(newObj);
+  console.log(newArr);
 } else {
   console.log("Error found");
 }
